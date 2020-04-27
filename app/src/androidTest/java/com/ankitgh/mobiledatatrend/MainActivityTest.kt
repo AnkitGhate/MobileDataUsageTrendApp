17e0f08e7059e5bff2226e038150ee94041fb8fc
ankitghcyb <ankitgh@cybage.com>
2020-04-27 14:33:22 +0530

    Minor cleanup

--- app/src/androidTest/java/com/ankitgh/mobiledatatrend/MainActivityTest.kt
+++ app/src/androidTest/java/com/ankitgh/mobiledatatrend/MainActivityTest.kt
@@ -1,5 +1,27 @@
 package com.ankitgh.mobiledatatrend
 
-import org.junit.Assert.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.ankitgh.mobiledatatrend.R.id.recyclerview
+import com.ankitgh.mobiledatatrend.R.id.rootView
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityTest
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @get : Rule
+    val mainActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityViewTest() {
+        onView(withId(rootView)).check(matches(isDisplayed()))
+        onView(withId(recyclerview)).check(matches(isDisplayed()))
+    }
+}
--- app/src/main/java/com/ankitgh/mobiledatatrend/MainActivity.kt
+++ app/src/main/java/com/ankitgh/mobiledatatrend/MainActivity.kt
@@ -15,7 +15,6 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
-
     private val TAG: String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
